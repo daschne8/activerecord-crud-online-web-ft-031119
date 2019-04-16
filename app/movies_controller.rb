8cc10e6b1b3a5fadd08d63c457970ae90730b000
Daniel Schneider <daschne8@gmail.com>
2019-04-16 19:13:50 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -7,9 +7,8 @@
 
 def can_be_instantiated_and_then_saved
   movie = Movie.new
-  movie.save
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
